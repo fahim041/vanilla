2f95ca20a2480717f773f7c5cf3aecfd9dcdc50b
Fahim090 <FAlSami@grantmatch.com>
2024-09-04 18:53:49 -0400

    toggle bug fixed

--- projects/typography-1/index.js
+++ projects/typography-1/index.js
@@ -1,27 +1,36 @@
 const themeSwitcher = document.querySelector('#theme-switcher');
 
 function darkMode() {
+  document.documentElement.setAttribute('data-theme', 'dark');
   themeSwitcher.children[0].classList.replace('fa-moon', 'fa-sun');
-  localStorage.setItem('theme', 'light');
-}
-
-function lightMode() {
-  themeSwitcher.children[0].classList.replace('fa-sun', 'fa-moon');
   localStorage.setItem('theme', 'dark');
 }
 
+function lightMode() {
+  document.documentElement.setAttribute('data-theme', 'light');
+  themeSwitcher.children[0].classList.replace('fa-sun', 'fa-moon');
+  localStorage.setItem('theme', 'light');
+}
+
+function onLoad() {
+  const savedTheme = localStorage.getItem('theme');
+  if (!savedTheme || savedTheme == 'light') {
+    lightMode();
+  } else {
+    darkMode();
+  }
+}
+
 function switchTheme() {
-  const currentTheme = document.documentElement.getAttribute('data-theme') || localStorage.getItem('theme');
+  const currentTheme = document.documentElement.getAttribute('data-theme');
 
   if (!currentTheme || currentTheme === 'light') {
-    document.documentElement.setAttribute('data-theme', 'dark');
     darkMode();
   } else {
-    document.documentElement.setAttribute('data-theme', 'light');
     lightMode();
   }
 }
 
 themeSwitcher.addEventListener('click', switchTheme);
 
-switchTheme();
+onLoad();
